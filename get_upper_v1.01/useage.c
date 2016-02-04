3ce6e03ef61f0469d8c29da85681d46fa799765c
ticotico <beranbus@gmail.com>
2016-02-04 12:04:55 +0800

    Create useage.c

---
+++ get_upper_v1.01/useage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void useage()
+{
+	printf("getleg (csv_output) v 1.01\n");
+	printf(" Useage: \n");
+	printf(" exe-name <nemo-filename> <total-nmf-number> <system> <strength> <noise> \n");
+	printf(" nemo-filename format: *.N.nmf expect .N.nmf \n");
+	printf(" total-nmf-number: number of nmf log file \n");
+	printf(" ststem: 3G or 4G \n");
+	printf(" strength: give a value to cuunt upper message. \n");
+	printf(" noise:  give a value to cuunt upper message. \n");
+	printf(" Edit by Bearanbus \n");
+	return ;
+}
+
