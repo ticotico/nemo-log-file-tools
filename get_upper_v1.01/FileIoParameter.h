a22720fc8f36aa23d9130a3e9368c05cc5ceecd5
ticotico <beranbus@gmail.com>
2016-02-04 12:04:02 +0800

    Create FileIoParameter.h

---
+++ get_upper_v1.01/FileIoParameter.h
@@ -0,0 +1,12 @@
+typedef struct {
+	char strengh_head[16];
+	int strength_col;
+	double gived_strength;
+	int len_strengh_head;
+	
+	char noise_head[16];
+	int noise_col;
+	double gived_noise;
+	int len_noise_head;
+	
+}FileIoParameter;
