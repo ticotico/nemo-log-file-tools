3d01390110b85f730c794dd5ef9bf6e3c07011c7
ticotico <beranbus@gmail.com>
2016-02-14 22:32:31 +0800

    Create log_line_io.h

---
+++ GPS_transfer_v1_01/log_line_io.h
@@ -0,0 +1,4 @@
+int getCol(char *dest, char *source, int n);
+void getfll(char *dest, const char *source_file, const char *message);
+void getall (const char *source_file, const char *message, char *dest_file);
+void getnll (const char *source_file, const char *message, char *dest_file);
