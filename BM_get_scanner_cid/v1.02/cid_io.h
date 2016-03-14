186cdc1ed2c9ff556855a4a838c902f6b34fdc15
ticotico <beranbus@gmail.com>
2016-03-14 21:20:37 +0800

    Create cid_io.h

---
+++ BM_get_scanner_cid/v1.02/cid_io.h
@@ -0,0 +1,6 @@
+#include "LTE_cid_array_io.h"
+#include "LTE_cid_dynamic_struct.h"
+#include "UMTS_cid_array_io.h"
+#include "UMTS_cid_dynamic_struct.h"
+void print_all_cid(LTE_cid *LTE_cid_struct, UMTS_cid *UMTS_cid_struct,
+				   const char *out_filename);
