fd874f52c49ce5171535fa307e339c4e78dedbe1
ticotico <beranbus@gmail.com>
2016-03-14 21:17:23 +0800

    Create LTE_cid_array_io.h

---
+++ BM_get_scanner_cid/v1.02/LTE_cid_array_io.h
@@ -0,0 +1,5 @@
+#include "LTE_cid_dynamic_struct.h"
+void ini_LTE_cid (LTE_cid *LTE_cid_struct);
+void add_LTE_cid (LTE_cid *LTE_cid_struct, int channel, long cid);
+void print_LTE_cid(LTE_cid *LTE_cid_struct);
+void close_LTE_cid(LTE_cid *LTE_cid_struct);
