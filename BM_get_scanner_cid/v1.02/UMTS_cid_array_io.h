40a366959d479e5e02b052e668b6e298c242ce44
ticotico <beranbus@gmail.com>
2016-03-14 21:19:26 +0800

    Create UMTS_cid_array_io.h

---
+++ BM_get_scanner_cid/v1.02/UMTS_cid_array_io.h
@@ -0,0 +1,5 @@
+#include "UMTS_cid_dynamic_struct.h"
+void ini_UMTS_cid (UMTS_cid *UMTS_cid_struct);
+void add_UMTS_cid (UMTS_cid *UMTS_cid_struct, int channel, char *cid);
+void print_UMTS_cid(UMTS_cid *UMTS_cid_struct);
+void close_UMTS_cid(UMTS_cid *UMTS_cid_struct);
